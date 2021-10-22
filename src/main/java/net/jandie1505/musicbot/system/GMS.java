d2a147ab6682524377843f39473ed86fd290b9b8
jandie1505 <jande1234567890@gmail.com>
2021-10-22 14:40:17 +0200

    Added commands

--- src/main/java/net/jandie1505/musicbot/system/GMS.java
+++ src/main/java/net/jandie1505/musicbot/system/GMS.java
@@ -13,6 +13,7 @@ import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.ErrorResponse;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.jandie1505.musicbot.MusicBot;
@@ -146,17 +147,97 @@ public class GMS {
 
                     CommandData nowplayingCommand = new CommandData("nowplaying", "Shows information about the song which is currently playing");
                     CommandData queueCommand = new CommandData("queue", "Shows the current queue");
+                    CommandData playCommand = new CommandData("play", "Play a song")
+                            .addOptions(new OptionData(OptionType.STRING, "song", "The song link / song name / playlist link you want to play").setRequired(true));
                     CommandData removeCommand = new CommandData("remove", "Remove a specific song from the queue")
                             .addOptions(new OptionData(OptionType.INTEGER, "index", "The index of the song you want to remove").setRequired(true));
                     CommandData searchCommand = new CommandData("search", "Search youtube")
                             .addOptions(new OptionData(OptionType.STRING, "query", "The text you want to search for"));
                     CommandData shuffleCommand = new CommandData("shuffle", "Shuffle the queue");
                     CommandData skipCommand = new CommandData("skip", "Skip a song")
-                            .addOptions(new OptionData(OptionType.INTEGER, "position", "Skip to a specific position"));
-                    CommandData playCommand = new CommandData("play", "Play a song")
-                            .addOptions(new OptionData(OptionType.STRING, "song", "The song link / song name / playlist link you want to play").setRequired(true));
+                            .addOptions(new OptionData(OptionType.INTEGER, "position", "Skip to a specific queue position"));
+                    CommandData removeUserCommand = new CommandData("removeuser", "Removes all songs by a specific member")
+                            .addOptions(new OptionData(OptionType.USER, "member", "The member you want to remove the music from").setRequired(true));
+                    CommandData forceskipCommand = new CommandData("forceskip", "Force skip a song")
+                            .addOptions(new OptionData(OptionType.INTEGER, "position", "Skip to a specific queue position"));
+                    CommandData movetrackCommand = new CommandData("movetrack", "Move a specific track in queue")
+                            .addOptions(new OptionData(OptionType.INTEGER, "from", "The track you want to move").setRequired(true))
+                            .addOptions(new OptionData(OptionType.INTEGER, "to", "The queue position you want to move the track to").setRequired(true));
+                    CommandData playnowCommand = new CommandData("playnow", "Stop the current song and play the specified song immediately")
+                            .addOptions(new OptionData(OptionType.STRING, "song", "The song you want to play").setRequired(true));
                     CommandData stopCommand = new CommandData("stop", "Stop playing music");
+                    CommandData volumeCommand = new CommandData("volume", "Change the volume")
+                            .addOptions(new OptionData(OptionType.INTEGER, "volume", "Change the volume to this value").setRequired(true));
                     CommandData leaveCommand = new CommandData("leave", "Leave the voice channel");
+
+                    SubcommandData mbsettingsInfoCommand = new SubcommandData("info", "See an overview of all settings");
+                    SubcommandData mbsettingsDJRoleCommand = new SubcommandData("djrole", "Add/remove/clear dj roles")
+                            .addOptions(new OptionData(OptionType.STRING, "action", "add/remove").setRequired(true).addChoice("add", "add").addChoice("remove", "remove").addChoice("clear", "clear"))
+                            .addOptions(new OptionData(OptionType.ROLE, "role", "Only required if you have chosen add/remove"));
+                    CommandData mbsettingsCommand = new CommandData("mbsettings", "Music bot settings command for administrators")
+                            .addSubcommands(mbsettingsInfoCommand)
+                            .addSubcommands(mbsettingsDJRoleCommand);
+
+                    g.upsertCommand(nowplayingCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(queueCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(playCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(removeCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(searchCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(shuffleCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(skipCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(removeUserCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(forceskipCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(movetrackCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(playnowCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(stopCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(volumeCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(leaveCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
+                    g.upsertCommand(mbsettingsCommand).queue(null, new ErrorHandler().handle(ErrorResponse.MISSING_ACCESS, e -> {
+                        leaveGuild(g.getId(), "Missing permissions for slash commands");
+                        Console.messageGMS("Left guild " + guildId + " for missing slash command permissions");
+                    }));
 
                     Console.messageGMS("Reloaded commands on guild " + g.getId());
                 } catch(PermissionException ignored) {}
