b187bb28c7d1b4c2beeafb0f3d452a852ca64c5b
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2022-09-18 14:19:21 +0200

    Permission requests

--- src/main/java/net/jandie1505/musicbot/slashcommands/AdminPermissionRequest.java
+++ src/main/java/net/jandie1505/musicbot/slashcommands/AdminPermissionRequest.java
@@ -4,18 +4,18 @@ import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.jandie1505.musicbot.MusicBot;
 import net.jandie1505.slashcommandapi.interfaces.SlashCommandPermissionRequest;
 
-public class UserPermissionRequest implements SlashCommandPermissionRequest {
+public class AdminPermissionRequest implements SlashCommandPermissionRequest {
 
     private final MusicBot musicBot;
 
-    public UserPermissionRequest(MusicBot musicBot) {
+    public AdminPermissionRequest(MusicBot musicBot) {
         this.musicBot = musicBot;
     }
 
     @Override
     public boolean hasPermission(SlashCommandInteraction interaction) {
         if(interaction.getMember() != null) {
-            return musicBot.getGMS().memberHasUserPermissions(interaction.getMember());
+            return musicBot.getGMS().memberHasAdminPermissions(interaction.getMember());
         } else {
             return false;
         }
--- src/main/java/net/jandie1505/musicbot/slashcommands/DJPermissionRequest.java
+++ src/main/java/net/jandie1505/musicbot/slashcommands/DJPermissionRequest.java
@@ -4,18 +4,18 @@ import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.jandie1505.musicbot.MusicBot;
 import net.jandie1505.slashcommandapi.interfaces.SlashCommandPermissionRequest;
 
-public class UserPermissionRequest implements SlashCommandPermissionRequest {
+public class DJPermissionRequest implements SlashCommandPermissionRequest {
 
     private final MusicBot musicBot;
 
-    public UserPermissionRequest(MusicBot musicBot) {
+    public DJPermissionRequest(MusicBot musicBot) {
         this.musicBot = musicBot;
     }
 
     @Override
     public boolean hasPermission(SlashCommandInteraction interaction) {
         if(interaction.getMember() != null) {
-            return musicBot.getGMS().memberHasUserPermissions(interaction.getMember());
+            return musicBot.getGMS().memberHasDJPermissions(interaction.getMember());
         } else {
             return false;
         }
