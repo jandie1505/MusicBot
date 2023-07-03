a2e31b405f14ff4d11c4a0198fe79db60c216c4b
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-07-03 23:32:40 +0200

    updated jda

--- src/main/java/net/jandie1505/musicbot/eventlisteners/EventsBasic.java
+++ src/main/java/net/jandie1505/musicbot/eventlisteners/EventsBasic.java
@@ -1,10 +1,10 @@
 package net.jandie1505.musicbot.eventlisteners;
 
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.ShutdownEvent;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ShutdownEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.jandie1505.musicbot.MusicBot;
 import net.jandie1505.musicbot.console.Console;
@@ -39,7 +39,12 @@ public class EventsBasic extends ListenerAdapter {
     }
 
     @Override
-    public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
+    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
+
+        if (event.getChannelLeft() == null) {
+            return;
+        }
+
         if(event.getMember() == event.getGuild().getSelfMember()) {
             this.musicBot.getMusicManager().disconnect(event.getGuild());
         }
